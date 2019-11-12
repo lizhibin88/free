724a7f3954be63384b0b5a6335fb382ca5578eba
lizhibin88 <35754445@qq.com>
2019-11-12 17:20:31 +0800

    free

---
+++ src/main/java/com/java/free/free/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.java.free.free.controller;
+
+import org.springframework.http.converter.json.GsonBuilderUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author lizhibin
+ * @create 2019-11-01 10:46
+ */
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
---
+++ src/test/java/com/java/free/free/FreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.free.free;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
